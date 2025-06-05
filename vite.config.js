61569ede7e59c785bd63ab7275c37ed18232e37b
Pastukh Mariia <mahka230@gmail.com>
2025-06-05 20:22:24 +0300

    Fix base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: '/portfolio/', // важливо! цей шлях — це імʼя вашого репозиторію
-  build: {
-    outDir: 'docs',     // щоб збиралося у docs/
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+	base: '/portfolio/',
+	build: {
+		outDir: 'docs',
+	},
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
 });
